436cfb25c3836cde05edabc67d7bb2e16e770a8a
shoaibsaleem0299 <shoaib0299@gmail.com>
2024-09-07 14:19:45 +0500

    add header and footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const footer = () => {
+  return (
+    <div className='bg-gray-700 text-white text-center text-3xl'>footer</div>
+  )
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const header = () => {
+  return (
+    <div className='text-4xl text-center'>header</div>
+  )
+}
